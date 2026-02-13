a01c4d50d61aa06c3b02466024712c4c2c27c96f
fabricioluna <fabricioluna@gmail.com>
2026-02-13 16:04:34 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error('Falha ao encontrar o elemento root');
+const firebaseConfig = {
+  apiKey: "AIzaSyBv...", // Substitua pela sua chave completa se necessário
+  authDomain: "monitor-virtual-fms.firebaseapp.com",
+  databaseURL: "https://monitor-virtual-fms-default-rtdb.firebaseio.com",
+  projectId: "monitor-virtual-fms",
+  storageBucket: "monitor-virtual-fms.appspot.com",
+  messagingSenderId: "833897320265",
+  appId: "1:833897320265:web:8c5166468498f828a1c977"
+};
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
